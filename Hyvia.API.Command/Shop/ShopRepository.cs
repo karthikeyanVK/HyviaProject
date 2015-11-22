68eda079785d7cd8a13ca3b262c51b9d0cb04b0f
Karthikeyan VK <karthik_3030@yahoo.com>
2015-11-22 22:40:58 +0530

    updated shops controller method

--- Hyvia.API.Command/Shop/ShopRepository.cs
+++ Hyvia.API.Command/Shop/ShopRepository.cs
@@ -5,6 +5,9 @@ using AutoMapper;
 using Hyvia.Data.Model;
 using Hyvia.Mongo.DataAccess;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System.Collections.Generic;
 
 namespace Hyvia.API.Command
 {
@@ -13,6 +16,17 @@ namespace Hyvia.API.Command
         public ShopRepository()
         {
             Mapper.CreateMap<ShopCommand, Shop>();
+            try
+            {
+                BsonClassMap.RegisterClassMap<Shop>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.SetIdMember(cm.GetMemberMap(x => x.ShopId).SetIdGenerator(StringObjectIdGenerator.Instance));
+                });
+                 
+            }
+            catch //Should figureout to check already registered
+            { }
         }
         public async Task<IList> GetShops(string shopName)
         {
@@ -23,6 +37,31 @@ namespace Hyvia.API.Command
                 searchData.SearchValue.Add(shopName);
             }
             return await AccessDb.GetListOf<BsonDocument>(searchData, MongoTables.ShopTableName);
+        }
+        public async Task<IList> GetShopsByProduct(string productId)
+        {
+            IList<SearchData> searchDataList = new List<SearchData>();
+            SearchData searchData = null;
+            if (!string.IsNullOrEmpty(productId))
+            {
+                searchData = new SearchData
+                {
+                    SearchField = "productId",
+                    SearchValue = new List<string> { productId }
+                };
+
+                searchDataList.Add(searchData);
+            }
+            var products = await AccessDb.GetListWithFilter<Product>(searchDataList, MongoTables.ProductTableName);
+            var shops = await AccessDb.GetListOf<Shop>(MongoTables.ShopTableName);
+
+            var shopDetails = from shop in shops
+                              join product in products on shop.ShopId equals productId into shopDetail
+                              select shop;
+
+
+            return shopDetails.ToList<Shop>();
+
         }
         public async Task<bool> Insertshop(ShopCommand shopCommand)
         {
--- Hyvia.API/Controllers/ShopController.cs
+++ Hyvia.API/Controllers/ShopController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Hyvia.API.Command;
 using MongoDB.Bson;
+using Cads.VelVenti.Api.Filters;
 
 namespace Hyvia.API
 {
@@ -16,10 +17,19 @@ namespace Hyvia.API
             _shopRepository = new ShopRepository();
         }
         // GET api/values/5
+        [CacheClient]
         public async Task<IList> Get(string shopName)
         {
             var relayResult = await _shopRepository.GetShops(shopName);
             return new[] { relayResult.ToJson() };
+        }
+        [Route("product")]
+        [HttpGet]
+        [CacheClient]
+        public async Task<IList> GetShopByProducts(string productId)
+        {
+            var result = await _shopRepository.GetShopsByProduct(productId);
+            return result;
         }
 
         // POST api/values
